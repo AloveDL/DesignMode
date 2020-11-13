aa2460e5d064ce2c6ecbfddf937b522c39b8152d
AloveDL <2763570837@qq.com>
2020-11-13 19:06:02 +0800

    init commit

--- KWIC/pipelines/Test.java
+++ KWIC/pipelines/Test.java
@@ -5,23 +5,6 @@ import java.util.Iterator;
 
 public class Test {
 	public static void main(String[] args) {
-//		LoadData loadData = new LoadData("KWIC\\\\pipelines\\\\text");
-//		loadData.run();
-//		Shifter shifter = new Shifter(loadData.getOutputList());
-//		shifter.run();
-//		Sort sort = new Sort(shifter.getOutputList());
-//		sort.run();
-//		SaveData saveData = new SaveData(sort.getOutputList(),"KWIC\\\\pipelines\\\\newtext");
-//		saveData.run();
-
-//		for(Iterator it =loadData.load("KWIC\\pipelines\\text").iterator();it.hasNext();){
-//			Object item = it.next();
-//			System.out.println(item);
-//		}
-//		for(Iterator it = sort.getOutputList().iterator();it.hasNext();){
-//			Object item = it.next();
-//			System.out.println(item);
-//		}
 
 			Pipelines pipelines = new Pipelines("KWIC\\pipelines\\text","KWIC\\pipelines\\newtext");
 			pipelines.run();
